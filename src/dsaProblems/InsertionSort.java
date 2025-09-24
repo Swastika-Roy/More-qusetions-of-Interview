3e329d62b5746f70796ddaa9f63e52b13a122394
Swastika Roy <swastikaroytitu@gmail.com>
2025-09-24 14:41:20 +0530

    Practicing

--- src/dsaProblems/InsertionSort.java
+++ src/dsaProblems/InsertionSort.java
@@ -5,15 +5,15 @@ import java.util.Arrays;
 
 public class InsertionSort {
     static void insertionSort(int arr[]){
-        for(int i=1;i<arr.length;i++) {
-            int j = i;
-            while (j > 0 && arr[j - 1] > arr[j]) {
-                int temp = arr[j - 1];
-                arr[j - 1] = arr[j];
-                arr[j] = temp;
-                j--;
-            }
-        }
+       for(int i=1; i < arr.length; i++){
+           int j= i;
+         while(j > 0 && arr[j-1] > arr[j]){
+             int temp = arr[j];
+             arr[j] = arr[j-1];
+             arr[j-1] = temp;
+             j--;
+         }
+       }
     }
 
     public static void main(String[] args) {
--- src/dsaProblems/bubbleSort.java
+++ src/dsaProblems/bubbleSort.java
@@ -9,15 +9,14 @@ public class bubbleSort {
         System.out.println(Arrays.toString(arr));
     }
     public static void BubbleSort(int[] arr){
-        for (int i=0;i< arr.length-1;i++){
-            for (int j=0; j < arr.length-1-i;j++){
-                if(arr[j+1] < arr[j] ){
+        for(int i=0; i < arr.length-1; i++){
+            for(int j=0; j < arr.length-1-i; j++){
+                if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
                 }
             }
         }
-
     }
 }
