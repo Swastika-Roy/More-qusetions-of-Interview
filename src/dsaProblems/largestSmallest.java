f8bfcfcd36922646e83781b9a0353ff353fd00da
Swastika Roy <swastikaroytitu@gmail.com>
2025-09-30 00:10:29 +0530

    larsmall

---
+++ src/dsaProblems/largestSmallest.java
@@ -0,0 +1,25 @@
+package dsaProblems;
+
+public class largestSmallest {
+    static int largest(int arr[]){
+        int lar = arr[0];
+        for (int i=0; i < arr.length; i++){
+            if(arr[i] > lar) lar = arr[i];
+        }
+        return lar;
+    }
+    static int smallest(int arr[]){
+        int small = arr[0];
+        for(int num : arr){
+            if(num < small) small= num;
+        }
+        return small;
+    }
+
+    static void main() {
+        int arr[] = {1,2,3,4,5};
+       int lar = largest(arr);
+       int small = smallest(arr);
+        System.out.println("largest = "+lar+" smallest = "+small);
+    }
+}
