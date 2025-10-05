3592ba9fceb75928315a75431158ec254ef6f4d2
Swastika Roy <swastikaroytitu@gmail.com>
2025-10-06 00:45:06 +0530

    revision

--- src/dsaProblems/quickSort.java
+++ src/dsaProblems/quickSort.java
@@ -40,28 +40,30 @@ public class quickSort {
         arr[high] = temp;
     }
 
-    static void quicksort(int arr[],int low,int high){
-        if(low < high){
-            int pv = partition(arr,low,high);
-            quicksort(arr,low,pv-1);
-            quicksort(arr,pv+1,high);
-        }
-    }
+   static void quicksort(int arr[],int low,int high){
+       if(low < high){
+           int pv = patition(arr,low,high);
+           quicksort(arr,low,pv-1);
+           quicksort(arr,pv+1,high);
+       }
+   }
 
-    static int partition(int arr[],int low,int high){
+   static int patition(int arr[],int low,int high){
         int pv = arr[low];
         int i = low+1;
-        int j=high;
+        int j = high;
 
         while (true){
-            while (i <= j && arr[i] <= pv) i++;
-            while (i <= j && arr[j] > pv ) j--;
+            while (i <= j && pv >= arr[i]) i++;
+            while (i <= j && pv < arr[j])   j--;
             if(i > j) break;
             swap(arr,i,j);
         }
+
         swap(arr,low,j);
+
         return j;
-    }
+   }
 
     public static void main(String[] args) {
         int arr[] = {9, 7, 3, 1, 5};
