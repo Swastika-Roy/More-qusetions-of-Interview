2cf0ab461efa2e0a301475d6544476fd73616c8f
Swastika Roy <swastikaroytitu@gmail.com>
2025-10-05 10:03:30 +0530

    revison

--- src/dsaProblems/quickSort.java
+++ src/dsaProblems/quickSort.java
@@ -5,39 +5,62 @@ import java.util.Arrays;
 public class quickSort {
 
     static void quickSort(int arr[]) {
-        quickSort(arr, 0, arr.length - 1);
+        quicksort(arr, 0, arr.length - 1);
     }
 
-    static void quickSort(int arr[], int low, int high) {
-        if (low < high) {
-            int pv = partition(arr, low, high);
-
-            //sort elements before and after partition
-            quickSort(arr, low, pv - 1);
-            quickSort(arr, pv + 1, high);
-        }
-    }
-
-    static int partition(int arr[], int low, int high) {
-        int pv = arr[low];
-        int i = low + 1;
-        int j = high;
-
-        while (true) {
-            while (i <= j && arr[i] <= pv) i++;
-            while (i <= j && arr[j] > pv) j--;
-            if (i > j) break;
-            swap(arr, i, j);
-        }
-
-        swap(arr, low, j);
-
-        return j;
-    }
+//    static void quickSort(int arr[], int low, int high) {
+//        if (low < high) {
+//            int pv = partition(arr, low, high);
+//
+//            //sort elements before and after partition
+//            quickSort(arr, low, pv - 1);
+//            quickSort(arr, pv + 1, high);
+//        }
+//    }
+//
+//    static int partition(int arr[], int low, int high) {
+//        int pv = arr[low];
+//        int i = low + 1;
+//        int j = high;
+//
+//        while (true) {
+//            while (i <= j && arr[i] <= pv) i++;
+//            while (i <= j && arr[j] > pv) j--;
+//            if (i > j) break;
+//            swap(arr, i, j);
+//        }
+//
+//        swap(arr, low, j);
+//
+//        return j;
+//    }
     static void swap(int arr[], int low, int high) {
         int temp = arr[low];
         arr[low] = arr[high];
         arr[high] = temp;
+    }
+
+    static void quicksort(int arr[],int low,int high){
+        if(low < high){
+            int pv = partition(arr,low,high);
+            quicksort(arr,low,pv-1);
+            quicksort(arr,pv+1,high);
+        }
+    }
+
+    static int partition(int arr[],int low,int high){
+        int pv = arr[low];
+        int i = low+1;
+        int j=high;
+
+        while (true){
+            while (i <= j && arr[i] <= pv) i++;
+            while (i <= j && arr[j] > pv ) j--;
+            if(i > j) break;
+            swap(arr,i,j);
+        }
+        swap(arr,low,j);
+        return j;
     }
 
     public static void main(String[] args) {
