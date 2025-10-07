adb712db242634c1dd2f9789d1507851f2f92bd7
Swastika Roy <swastikaroytitu@gmail.com>
2025-10-07 11:04:13 +0530

    rev

--- src/dsaProblems/mergeSort.java
+++ src/dsaProblems/mergeSort.java
@@ -84,36 +84,36 @@ static void print(int arr[]){
         print(arr);
     }
 
-    static void mergeSort(int arr[]){
-    if(arr.length <= 1) return;
-
+   static void mergeSort(int arr[]){
+    if (arr.length <= 1) return;
     int mid = arr.length/2;
     int left[] = new int[mid];
     int right[] = new int[arr.length-mid];
 
-    for (int i=0; i < mid; i++){
+    for (int i=0; i < mid;i++){
         left[i] = arr[i];
     }
 
-    for (int i=mid; i < arr.length; i++){
+    for (int i=mid; i<arr.length; i++ ){
         right[i-mid] = arr[i];
     }
 
     mergeSort(left);
     mergeSort(right);
-    merge(arr,left,right);
-    }
 
-    static void merge(int arr[],int left[], int right[]){
-    int i=0; int j=0; int k=0;
+    merge(arr,left,right);
+   }
+
+   static void merge(int arr[],int left[],int right[]){
+    int i=0;int j=0;int k=0;
 
     while (i < left.length && j < right.length){
-        if(left[i] < right[j]) arr[k++] =left[i++];
+        if(left[i] < right[j]) arr[k++] = left[i++];
         else arr[k++] = right[j++];
     }
 
     while (i < left.length) arr[k++] = left[i++];
     while (j < right.length) arr[k++] = right[j++];
-    }
+   }
 
 }
