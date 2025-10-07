9b5a0d873191b586f6d908f2ff08206fad7da7dc
Swastika Roy <swastikaroytitu@gmail.com>
2025-10-08 00:29:40 +0530

    revision

--- src/dsaProblems/mergeSort.java
+++ src/dsaProblems/mergeSort.java
@@ -85,35 +85,31 @@ static void print(int arr[]){
     }
 
    static void mergeSort(int arr[]){
-    if (arr.length <= 1) return;
-    int mid = arr.length/2;
-    int left[] = new int[mid];
-    int right[] = new int[arr.length-mid];
-
-    for (int i=0; i < mid;i++){
-        left[i] = arr[i];
-    }
-
-    for (int i=mid; i<arr.length; i++ ){
-        right[i-mid] = arr[i];
-    }
-
-    mergeSort(left);
-    mergeSort(right);
-
-    merge(arr,left,right);
+   if(arr.length <= 1) return;
+   int mid = arr.length/2;
+   int left[] = new int[mid];
+   int right[] = new int[arr.length-mid];
+   for(int i=0; i < mid; i++){
+       left[i] = arr[i];
+   }
+   for (int i=mid; i < arr.length;i++){
+       right[i-mid]=arr[i];
+   }
+   mergeSort(left);
+   mergeSort(right);
+   merge(arr,left,right);
    }
 
    static void merge(int arr[],int left[],int right[]){
-    int i=0;int j=0;int k=0;
-
-    while (i < left.length && j < right.length){
-        if(left[i] < right[j]) arr[k++] = left[i++];
-        else arr[k++] = right[j++];
-    }
-
-    while (i < left.length) arr[k++] = left[i++];
-    while (j < right.length) arr[k++] = right[j++];
+        int i=0;
+        int j=0;
+        int k=0;
+        while(i < left.length && j < right.length){
+            if(left[i] < right[j]) arr[k++] = left[i++];
+            else arr[k++] = right[j++];
+        }
+        while (i < left.length )arr[k++] = left[i++];
+       while (j < right.length )arr[k++] = right[j++];
    }
 
 }
--- src/dsaProblems/quickSort.java
+++ src/dsaProblems/quickSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class quickSort {
 
     static void quickSort(int arr[]) {
-        quicksort(arr, 0, arr.length - 1);
+        quickSort(arr, 0, arr.length - 1);
     }
 
 //    static void quickSort(int arr[], int low, int high) {
@@ -40,30 +40,27 @@ public class quickSort {
         arr[high] = temp;
     }
 
-   static void quicksort(int arr[],int low,int high){
-       if(low < high){
-           int pv = patition(arr,low,high);
-           quicksort(arr,low,pv-1);
-           quicksort(arr,pv+1,high);
-       }
-   }
+ static void quickSort(int arr[],int low,int high){
+        if(low < high){
+            int pv = partition(arr,low,high);
+            quickSort(arr,low,pv-1);
+            quickSort(arr,pv+1,high);
+        }
+ }
 
-   static int patition(int arr[],int low,int high){
+ static int partition(int arr[],int low,int high){
         int pv = arr[low];
         int i = low+1;
         int j = high;
-
         while (true){
-            while (i <= j && pv >= arr[i]) i++;
-            while (i <= j && pv < arr[j])   j--;
-            if(i > j) break;
+            while (i <= j && arr[i] <= pv)i++;
+                while (i <= j && arr[j] > pv)j--;
+                if(i > j) break;
             swap(arr,i,j);
         }
-
         swap(arr,low,j);
-
         return j;
-   }
+ }
 
     public static void main(String[] args) {
         int arr[] = {9, 7, 3, 1, 5};
