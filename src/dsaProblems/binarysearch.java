62ad30a2ccf3f4a644aacf36e076550707e4d00e
Swastika Roy <swastikaroytitu@gmail.com>
2025-10-12 18:41:21 +0530

    revision

--- src/dsaProblems/binarysearch.java
+++ src/dsaProblems/binarysearch.java
@@ -1,9 +1,30 @@
 package dsaProblems;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class binarysearch {
     static void main() {
-       int arr[] = {1,2,3,4};
-        System.out.println(binary(arr,4));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter size of array : ");
+        int n=sc.nextInt();
+      int arr[] = new int[n];
+
+        System.out.println("Enter elements : ");
+      for(int  i=0; i < n; i++){
+          arr[i] = sc.nextInt();
+      }
+        System.out.println("enter target");
+      int x = sc.nextInt();
+        System.out.println(Arrays.toString(arr));
+        System.out.println(binary(arr,x));
+
+    }
+
+    static void print(int arr[]){
+        for (int i=0;i < arr.length; i++){
+            System.out.print(arr[i]);
+        }
     }
 
     static int binary(int arr[],int target){
