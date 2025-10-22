6bf599a7017179db1aca22b43ae27a04ddfbc2b4
Swastika Roy <swastikaroytitu@gmail.com>
2025-10-23 00:45:58 +0530

    done

---
+++ src/dsaProblems/TransposeMatrix.java
@@ -0,0 +1,37 @@
+package dsaProblems;
+
+import java.util.Arrays;
+
+public class TransposeMatrix {
+
+    static int[][] transpose(int arr[][]) {
+        int row = arr.length;
+        int col = arr[0].length;
+        int ans[][] = new int[col][row];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                ans[j][i] = arr[i][j];
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int arr[][] = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        int[][] transposed = transpose(arr);
+
+        // Option 1: Print using deepToString
+        System.out.println(Arrays.deepToString(transposed));
+
+//        // Option 2: Print matrix neatly
+//        for (int[] row : transposed) {
+//            System.out.println(Arrays.toString(row));
+//        }
+    }
+}
