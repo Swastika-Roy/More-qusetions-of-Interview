ba57bb61047e2d35d4f4946b7bd1ac5ff8c672fc
Swastika Roy <swastikaroytitu@gmail.com>
2025-11-04 18:56:02 +0530

    done

--- src/dsaProblems/isPerfectSquare.java
+++ src/dsaProblems/isPerfectSquare.java
@@ -7,11 +7,11 @@ public class isPerfectSquare {
     }
 
     static boolean validPerfect(int x){
-        long st=0, end = x;
+        long st=0; long end = x;
         while (st <= end){
             long mid=st+(end-st)/2;
             if(mid*mid == x)return true;
-            else if(mid*mid < x)st = mid+1;
+            else if(mid*mid < x)st=mid+1;
             else end=mid-1;
         }
         return false;
