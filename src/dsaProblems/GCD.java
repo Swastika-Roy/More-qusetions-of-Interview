8e86f6d975f1c9d42957440fa92b24a61499ab02
Swastika Roy <swastikaroytitu@gmail.com>
2025-11-06 20:54:40 +0530

    rev

--- src/dsaProblems/GCD.java
+++ src/dsaProblems/GCD.java
@@ -2,12 +2,12 @@ package dsaProblems;
 
 public class GCD {
     public static int gcd(int a,int b){
-       while(b != 0){
-           int temp = b;
-           b = a%b;
-           a = temp;
-       }
-       return a;
+     while (b!=0){
+         int temp = b;
+         b = a%b;
+         a = temp;
+     }
+     return a;
     }
 
     public static void main(String[] args) {
