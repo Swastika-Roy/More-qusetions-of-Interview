e5af4fcfe196fbdd3ba8e7ecc4628f65f67c7cb4
Swastika Roy <swastikaroytitu@gmail.com>
2025-11-09 18:51:36 +0530

    find missing num

--- src/dsaProblems/findMissingNum.java
+++ src/dsaProblems/findMissingNum.java
@@ -6,10 +6,12 @@ public class findMissingNum {
         System.out.println(missing(arr));
     }
     static int missing(int arr[]){
-        int n = arr.length+1;
-        int total = n*(n+1)/2;
-        int arrsum = 0;
-        for(int num : arr) arrsum += num;
-        return total-arrsum;
+       int n=arr.length+1;
+       int total=n*(n+1)/2;
+       int arrsum =0;
+       for (int i=0; i < arr.length;i++){
+           arrsum += arr[i];
+       }
+       return total-arrsum;
     }
 }
