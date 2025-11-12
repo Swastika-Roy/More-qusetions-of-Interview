c7836f346d615b1c7e4f340bf0f4fb5a72eb350d
Swastika Roy <swastikaroytitu@gmail.com>
2025-11-12 21:05:58 +0530

    practice

--- src/dsaProblems/FindLargest.java
+++ src/dsaProblems/FindLargest.java
@@ -14,4 +14,17 @@ public class FindLargest {
         int arr[] = {1,2,3,4,5};
         largest(arr);
     }
+
+//    int i = m - 1;      // last index of arr1's initial elements
+//    int j = n - 1;      // last index of arr2
+//    int k = m + n - 1;  // last index of merged array
+//
+//    // Merge from the end into arr1
+//        while (j >= 0) {
+//        if (i >= 0 && arr1[i] > arr2[j]) {
+//            arr1[k--] = arr1[i--];
+//        } else {
+//            arr1[k--] = arr2[j--];
+//        }
+//    }
 }
--- src/dsaProblems/MergeTwoArrays.java
+++ src/dsaProblems/MergeTwoArrays.java
@@ -5,17 +5,18 @@ import java.util.Arrays;
 public class MergeTwoArrays {
     // Merge arr2 into arr1 (which has extra space at the end)
     public static void mergeSorted(int[] arr1, int m, int[] arr2, int n) {
-        int i = m - 1;      // last index of arr1's initial elements
-        int j = n - 1;      // last index of arr2
-        int k = m + n - 1;  // last index of merged array
+       int i=m-1;
+       int j=n-1;
+        int k = m+n-1;
 
-        // Merge from the end into arr1
-        while (j >= 0) {
-            if (i >= 0 && arr1[i] > arr2[j]) {
-                arr1[k--] = arr1[i--];
-            } else {
-                arr1[k--] = arr2[j--];
+        while(j >= 0){{
+            if(i >= 0 && arr1[i] > arr2[j]){
+                arr1[k--]=arr1[i--];
+            }else{
+                arr1[k--]=arr2[j--];
             }
+        }
+
         }
     }
 
@@ -28,11 +29,13 @@ public class MergeTwoArrays {
         arr1[3] = 4;
         arr1[4] = 5;
 
-        int arr2[] = {4, 5, 6, 7, 8};
+        int arr2[] = {9, 5, 6, 7, 8};
 
         mergeSorted(arr1, m, arr2, n); // merge second array into first
 
         // Print merged result
         System.out.println(Arrays.toString(arr1));
     }
+
+
 }
