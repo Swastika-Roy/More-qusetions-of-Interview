8e1afd0e74d92c9ded567933214d5ca6cc09ab6d
Swastika Roy <swastikaroytitu@gmail.com>
2025-11-16 13:22:35 +0530

    reverse number

--- src/dsaProblems/ReverseNum.java
+++ src/dsaProblems/ReverseNum.java
@@ -3,12 +3,12 @@ package dsaProblems;
 public class ReverseNum {
 
     public static void Rev(int num){
-        int sum = 0; int temp = num;
-        while(temp != 0){
-            int rem = temp % 10;
-            sum = sum*10 + rem;
-            temp /= 10;
-        }
+       int temp = num; int sum=0;
+       while(temp != 0){
+           int rem = temp%10;
+           sum = sum*10+rem;
+           temp /=10;
+       }
         System.out.println(sum);
     }
 
