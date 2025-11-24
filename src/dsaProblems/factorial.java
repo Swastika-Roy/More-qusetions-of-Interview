bf7970e82e71a06a9d62107a8349a8a496361606
Swastika Roy <swastikaroytitu@gmail.com>
2025-11-25 00:14:44 +0530

    done

--- src/dsaProblems/factorial.java
+++ src/dsaProblems/factorial.java
@@ -2,11 +2,11 @@ package dsaProblems;
 
 public class factorial {
     public static void main(String[] args) {
-        int fact = 1;
-        int n = 5;
-        for(int i = 1;i<=n;i++){
-            fact *= i;
-        }
+      int fact = 1;
+      int n=5;
+      for(int i=1; i <=n; i++){
+          fact *= i;
+      }
         System.out.println(fact);
     }
 }
