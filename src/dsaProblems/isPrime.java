448d044fa5c5a5f655b35ebd2e1680e848ecffe0
Swastika Roy <swastikaroytitu@gmail.com>
2025-11-26 21:58:28 +0530

    done

--- src/dsaProblems/isPrime.java
+++ src/dsaProblems/isPrime.java
@@ -2,12 +2,12 @@ package dsaProblems;
 
 public class isPrime {
     static void isPrime(int n) {
-        int count = 0;
-        for(int i=2; i < n; i++){
-            if(n%i==0) count++;
-        }
-        if (count == 2) System.out.println("Prime");
-        else System.out.println("Composite");
+       int count=0;
+       for (int i=2; i < n; i++){
+           if(n%i==0)count++;
+       }
+        if(count == 2) System.out.println("prime");
+        else System.out.println("composite");
     }
 
     public static void main(String[] args) {
