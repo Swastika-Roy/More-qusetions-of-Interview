7f0a805b9b1ab19383b5a904ac31a53815a395d4
Swastika Roy <swastikaroytitu@gmail.com>
2025-12-02 22:09:56 +0530

    git push origin main

--- src/dsaProblems/FindLargest.java
+++ src/dsaProblems/FindLargest.java
@@ -2,13 +2,14 @@ package dsaProblems;
 
 public class FindLargest {
     public static void largest(int[] arr){
-        int largest = arr[0];
-        for(int num : arr){
-            if(num > largest){
-                 largest = num;
-            }
-        }
-        System.out.println("Largest : "+largest);
+       int l = arr[0];
+       for(int num : arr){
+           if(num > l){
+               l=num;
+           }
+       }
+        System.out.println(l);
+       return;
     }
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5};
