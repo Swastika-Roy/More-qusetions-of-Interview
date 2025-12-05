d4f96d945f2d06faf890b8139577cee545f2ba17
Swastika Roy <swastikaroytitu@gmail.com>
2025-12-05 22:58:26 +0530

    done

--- src/dsaProblems/StringRotation.java
+++ src/dsaProblems/StringRotation.java
@@ -3,9 +3,10 @@ package dsaProblems;
 public class StringRotation {
 
     static boolean rotate(String s1,String s2){
-        if(s1.length() != s2.length()) return false;
-        String s3 = s1+s1;
-        return s3.contains(s2);
+       if(s1.length() != s2.length())return false;
+       String s3 = s1+s2;
+       if (s3.contains(s2)) return true;
+       else return false;
     }
 
     static void main() {
