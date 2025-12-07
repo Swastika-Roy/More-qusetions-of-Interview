7d7b4d21007b57e18f7d3f7494a5799cc35bb482
Swastika Roy <swastikaroytitu@gmail.com>
2025-12-07 20:04:44 +0530

    done

--- src/dsaProblems/SortArray.java
+++ src/dsaProblems/SortArray.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class SortArray {
     public static void main(String[] args) {
       int arr[] = {9,8,7,2,1};
-      Arrays.sort(arr);
+    Arrays.sort(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
