1c86cf5dd272ab1b4cd87af562da898f277fa8fb
Swastika Roy <swastikaroytitu@gmail.com>
2025-12-09 18:18:05 +0530

    sumOfDigits

--- src/dsaProblems/SumOfDigits.java
+++ src/dsaProblems/SumOfDigits.java
@@ -2,13 +2,13 @@ package dsaProblems;
 
 public class SumOfDigits {
     static int sum(int num){
-        int sum =0;
-        while(num > 0){
-            int r = num%10;
-            sum += r;
-            num /= 10;
-        }
-        return sum;
+    int sum = 0;
+    while(num != 0){
+        int r = num %10;
+        sum += r;
+        num /= 10;
+    }
+    return sum;
     }
     static void main() {
         System.out.println(sum(123));
