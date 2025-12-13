735f23bd10e95dff8bd25d3b0e986afa75461bac
Swastika Roy <swastikaroytitu@gmail.com>
2025-12-13 18:22:33 +0530

    sorting

--- src/dsaProblems/SortArray.java
+++ src/dsaProblems/SortArray.java
@@ -2,8 +2,8 @@ package dsaProblems;
 import java.util.Arrays;
 public class SortArray {
     public static void main(String[] args) {
-      int arr[] = {1,2,9,8,2};
-       Arrays.sort(arr);
-       System.out.println(Arrays.toString(arr));
+    int arr[] = {5,6,7,1,2};
+    Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
